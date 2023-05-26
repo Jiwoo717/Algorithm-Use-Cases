1cdb09cf6bb4579852ce10f417b61b9be28c2242
Jiwoo717 <kimeric717@gmail.com>
2023-05-25 20:07:03 -0700

    second problem

--- Source Analysis/Platform Source Analysis.sql
+++ Source Analysis/Platform Source Analysis.sql
@@ -4,7 +4,7 @@
     --Average Order Value by Source
     WITH DistinctOrders AS (
         SELECT DISTINCT EXTERNAL_ORDER_ID, ORDER_SOURCE, TOTALS
-        FROM WISELY_WAREHOUSE.WISELY_SHARE_V2.ORDERS
+        FROM WAREHOUSE.ORDERS
         WHERE TIME_PLACED BETWEEN '2022-11-01' AND '2023-10-01'
     )
 
@@ -15,3 +15,36 @@
         DistinctOrders
     GROUP BY
         ORDER_SOURCE;
+
+
+-- Get the Total Payments by Month using the same segmentation (Source)
+    -- Total Payments Monthly by Source
+    WITH DistinctOrders AS (
+    SELECT DISTINCT EXTERNAL_ORDER_ID, ORDER_SOURCE, PAYMENTS, TIME_PLACED
+    FROM WAREHOUSE.ORDERS
+    WHERE TIME_PLACED BETWEEN '2023-1-01' AND '2023-05-01'
+)
+
+, PaymentsFlattened AS (
+    SELECT
+        ORDER_SOURCE,
+        EXTRACT(MONTH FROM TIME_PLACED) AS month,
+        EXTRACT(YEAR FROM TIME_PLACED) AS year,
+        PAYMENT.value:"amount"::FLOAT AS payment_amount
+    FROM
+        DistinctOrders,
+        LATERAL FLATTEN(input => DistinctOrders.PAYMENTS) AS PAYMENT
+)
+
+SELECT
+    ORDER_SOURCE,
+    month,
+    year,
+    TO_VARCHAR(month, '00') || '-' || TO_VARCHAR(year) AS business_date,
+    SUM(payment_amount) AS total_payments
+FROM
+    PaymentsFlattened
+GROUP BY
+    ORDER_SOURCE, month, year
+ORDER BY
+    ORDER_SOURCE, year, month;
